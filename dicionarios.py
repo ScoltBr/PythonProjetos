906a414aad3d21699fa4051f06e9980f69e0e7f3
Luiz Eduardo de Oliveira <totosantinho2000@hotmail.com>
2020-02-28 01:05:01 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -137,10 +137,10 @@ print(receita)
 
 # Se a chave não existir será gerado um KeyError
 # OBS: Neste caso o valor removido não é retornado
-"""
+
 
 # Imagine que você tem um comércio eletronico, onde temos um carrinho de compras na qual adicionamos produtos.
-"""
+
 Carrinho de Compras:
     Produto 1:
        - nome:
@@ -150,7 +150,6 @@ Carrinho de Compras:
        - nome:
        - quantidade:
        - preço:
-"""
 
 # 1 - Poderiamos utilizar uma Lista para isso? Sim
 
@@ -185,3 +184,66 @@ carrinho.append(produto1)
 carrinho.append(produto2)
 
 print(carrinho)
+
+# Desta forma, facilmente adicionamos ou removemos produtos no carrinho e em cada produto
+# podemos ter a certeza sobre cada informação.
+
+# Métodos de dicionários.
+
+d = dict(a=1, b=2 , c=3)
+
+print(d)
+print(type(d))
+
+# Limpar o dicionário (Zerar dados)
+d.clear()
+
+print(d)
+
+# Copiando um dicionário para outro
+
+# Forma 1 # Deep Copy
+
+novo = d.copy()
+
+print(novo)
+
+novo['d'] = 4
+
+print(d)
+print(novo)
+
+# Forma 2 # Shallow Copy
+
+novo = d
+
+print(novo)
+
+novo['d'] = 4
+
+print(d)
+print(novo)
+
+"""
+
+# Forma não usual de criação de dicionários
+
+outro = {}.fromkeys('a', 'b')
+
+print(outro)
+print(type(outro))
+
+usuario = {}.fromkeys(['nome', 'pontos', 'email', 'profile'], 'desconhecido')
+print(usuario)
+print(type(usuario))
+
+# O método fromkeys recebe dois parâmetros: um interável e um valor
+# Ele vai gerar para cada valor um iterável uma chave e irá atribuir a esta chave o valor informado.
+
+veja = {}.fromkeys('teste', 'valor')
+print(veja)
+
+veja = {}.fromkeys(range(1 ,11), 'novo')
+
+print(veja)
+
