8a759424ea891f505f681c3495b6b7806a892a06
Luiz Eduardo <totosantinho2000@hotmail.com>
2020-10-09 11:17:47 -0300

    Update definir_função.py

--- definir_função.py
+++ definir_função.py
@@ -58,4 +58,62 @@ nome_da_função -> SEMPRE, com letra minúscula, e se for nome composto, separa
 parametros_de_entrada -> Opcional, onde tede mais de um, cada um separado por virgula (','), podendo ser opcionais ou não;
 bloco_da_função -> Também chamado de corpo da função ou implementação, é onde o processamento da função acontece;
 Nesse bloco, pode ter ou não retorno da função
+
+OBS: Veja que para definir uma função, utilizamos a palavra reservada 'def' informando ao Python que estamos
+definindo uma função. Também abrimos o bloco de código com o já conhecido dois pontos (':') que é utilizado
+em Python para definir blocos. 
 """
+
+#Definindo a primeira função
+
+# Definição
+
+def diz_oi():
+    print('Oi!')
+
+"""
+OBS:
+
+1 - Veja que, dentro das nossas funções podemos utilizar outras funçoes;
+2 - Veja que nossa função só executa 1 tarefa, ou seja, a única coisa que ela fez é dizer oi;
+3 - Veja que esta função não recebe nenhum parâmetro de entrada;
+4 - Veja que esta função não retorna nada;
+"""
+
+# Utilizado funções
+
+# Chamada de execução
+# diz_oi()
+
+"""
+Atenção:
+
+Nunca esqueça de utilizar os parènteses ao executar uma função.
+
+Exemplo:
+# Errado!
+diz_oi
+
+# Certo
+diz_oi()
+
+# Errado
+diz_oi ()
+"""
+
+# Exemplo 2
+
+def cantar_parabens():
+    printf('Parabéns para você')
+    printf('Nesta data querida')
+    printf('Muitas felicidades')
+    printf('Muitos anos de vida')
+
+#for n in range(4):
+#    cantar_parabens()
+
+# Em python, podemos inclusive criar variáveis do tipo de uma função e executar esta função através da varável
+
+cantar = cantar_parabens()
+
+cantar()
